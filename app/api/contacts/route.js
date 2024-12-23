b32d41291a3c46ae8e82cc0414d3a7c1f8ba5ae5
SaqAsh <121772695+SaqAsh@users.noreply.github.com>
2024-12-22 20:19:40 -0500

    file cleanup and initial commit

---
+++ app/api/contacts/route.js
@@ -0,0 +1,28 @@
+export async function GET(request) {
+  const contactMediums = [
+    {
+      medium: "github",
+      username: "SaqAsh",
+      link: "https://github.com/SaqAsh",
+    },
+    {
+      medium: "email",
+      username: "s5ashraf@uwaterloo.ca",
+      link: "mailto:s5ashraf@uwaterloo.ca",
+    },
+    {
+      medium: "linkedin",
+      username: "saqib5ashraf",
+      link: "https://www.linkedin.com/in/saqib5ashraf/",
+    },
+  ];
+
+  return Response.json(contactMediums, {
+    status: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    },
+  });
+}
